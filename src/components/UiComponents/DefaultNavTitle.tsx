5950c040e6380ed9c882858fa51620f241d867d8
Daniel <rotarudaniel567@gmail.com>
2024-04-26 11:40:26 +0300

    bug fixes

--- src/components/UiComponents/DefaultNavTitle.tsx
+++ src/components/UiComponents/DefaultNavTitle.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import "./DefaultNavTitle.scss"
 
 export const DefaultNavTitle = () => {
--- src/components/UiComponents/NavComponentTitle.tsx
+++ src/components/UiComponents/NavComponentTitle.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import './NavComponentTitle.scss'
 
 type NavCompoentTitleProps = {
